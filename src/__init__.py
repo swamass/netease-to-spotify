34a9587f37dda536b3883e4ac1743c5ba6bd44a4
swamass <borang1996@gmail.com>
2026-09-14 23:25:42 +0800

    Apply artist-bound rank consensus fallback

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
 from . import spotify as spotify
 from .matcher_policy import apply as _apply_matcher_policy
+from .artist_bound_rank_fallback import apply as _apply_artist_bound_rank_fallback
 
 _apply_matcher_policy(spotify)
+_apply_artist_bound_rank_fallback(spotify)
