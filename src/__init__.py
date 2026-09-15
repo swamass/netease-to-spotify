7b7d49ca99b820b4b71cf7238d30a7b034739711
swamass <borang1996@gmail.com>
2026-09-15 11:27:19 +0800

    Enable cross-language second-query retrieval fallback

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from . import spotify as spotify
 from .matcher_policy import apply as _apply_matcher_policy
 from .artist_bound_rank_fallback import apply as _apply_artist_bound_rank_fallback
+from .cross_language_retrieval import apply as _apply_cross_language_retrieval
 
 _apply_matcher_policy(spotify)
 _apply_artist_bound_rank_fallback(spotify)
+_apply_cross_language_retrieval(spotify)
