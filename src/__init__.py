5b909e66525142262e6bf69939427d1e12315d60
swamass <borang1996@gmail.com>
2026-09-15 11:23:41 +0800

    Apply retrieval fallback before rank consensus policy

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from . import spotify as spotify
 from .matcher_policy import apply as _apply_matcher_policy
+from .retrieval_policy import apply as _apply_retrieval_policy
 from .artist_bound_rank_fallback import apply as _apply_artist_bound_rank_fallback
 
 _apply_matcher_policy(spotify)
+_apply_retrieval_policy(spotify)
 _apply_artist_bound_rank_fallback(spotify)
