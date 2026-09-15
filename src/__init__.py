f5d0f8688771caae5326c9750d97baae73b67ea3
swamass <borang1996@gmail.com>
2026-09-15 11:25:42 +0800

    Apply cross-language retrieval policy

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from . import spotify as spotify
 from .matcher_policy import apply as _apply_matcher_policy
 from .artist_bound_rank_fallback import apply as _apply_artist_bound_rank_fallback
+from .retrieval_policy import apply as _apply_retrieval_policy
 
 _apply_matcher_policy(spotify)
 _apply_artist_bound_rank_fallback(spotify)
+_apply_retrieval_policy(spotify)
